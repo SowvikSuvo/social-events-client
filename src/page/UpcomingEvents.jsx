2dbb4635003ef62035af949207ff37035d694708
SowvikSuvo <skarmoker44@gmail.com>
2025-11-10 15:06:03 +0600

    added search bar implement on upcoming page

--- src/page/UpcomingEvents.jsx
+++ src/page/UpcomingEvents.jsx
@@ -1,22 +1,81 @@
 import { CalendarDays, MapPin, Tag } from "lucide-react";
 import React from "react";
+import { useState } from "react";
 import { Link, useLoaderData } from "react-router";
 
 const UpcomingEvents = () => {
   const data = useLoaderData();
-  console.log(data);
+  const [events, setEvents] = useState(data);
+  const [loading, setLoading] = useState(false);
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    const search_text = e.target.search.value;
+    console.log(search_text);
+
+    fetch(`http://localhost:3000/search?search=${search_text}`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setEvents(data);
+        setLoading(false);
+      });
+  };
+
+  if (loading) {
+    return (
+      <div className="flex justify-center items-center">
+        <span className="loading loading-infinity loading-xl"></span>
+      </div>
+    );
+  }
 
   return (
     <div className="max-w-7xl mx-auto px-4 py-10">
       <h2 className="text-3xl font-bold text-center mb-8">
         Upcoming Events: <span className="text-secondary">{data.length}</span>
+        <div className="">
+          <div>
+            <form
+              onSubmit={handleSearch}
+              className="text-center flex justify-center items-center"
+            >
+              <label className="input rounded-full mb-5 mt-5 ">
+                <svg
+                  className="h-[1em] opacity-50"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                >
+                  <g
+                    strokeLinejoin="round"
+                    strokeLinecap="round"
+                    strokeWidth="2.5"
+                    fill="none"
+                    stroke="currentColor"
+                  >
+                    <circle cx="11" cy="11" r="8"></circle>
+                    <path d="m21 21-4.3-4.3"></path>
+                  </g>
+                </svg>
+                <input name="search" type="search" placeholder="Search" />
+              </label>
+              <button
+                type="submit"
+                className="btn btn-secondary py-1  rounded-full"
+              >
+                {loading ? "Searching....." : "Search"}
+              </button>
+            </form>
+          </div>
+          <div>filter sort</div>
+        </div>
       </h2>
 
       {data.length === 0 ? (
         <p className="text-center text-gray-500">No upcoming events found.</p>
       ) : (
         <div className="grid gap-8 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
-          {data.map((event) => (
+          {events.map((event) => (
             <div
               key={event._id}
               className="group bg-white shadow-lg rounded-2xl overflow-hidden transition-transform transform hover:-translate-y-2 hover:shadow-2xl border border-gray-100"
